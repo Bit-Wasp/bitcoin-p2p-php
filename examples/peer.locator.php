c6e792b414dbf8977f2d37853ed6ee5d029b3053
Thomas Kerin <afk11@users.noreply.github.com>
2015-07-13 23:06:29 +0100

    Tweak peer.locator example

--- examples/peer.locator.php
+++ examples/peer.locator.php
@@ -2,44 +2,28 @@
 
 require "../vendor/autoload.php";
 
-use BitWasp\Bitcoin\Networking\P2P\PeerLocator;
-use BitWasp\Bitcoin\Networking\MessageFactory;
-use BitWasp\Bitcoin\Networking\Structure\NetworkAddress;
-use BitWasp\Bitcoin\Bitcoin;
 use BitWasp\Bitcoin\Networking\P2P\Peer;
-use BitWasp\Buffertools\Buffer;
 
 $loop = React\EventLoop\Factory::create();
+$factory = new \BitWasp\Bitcoin\Networking\Factory($loop);
+$dns = $factory->getDns();
 
-$dnsResolverFactory = new \BitWasp\Bitcoin\Networking\Dns\Factory();
-$dns = $dnsResolverFactory->createCached('8.8.8.8', $loop);
-$connector = new React\SocketClient\Connector($loop, $dns);
+$peerFactory = $factory->getPeerFactory();
+$connector = $peerFactory->getConnector($dns);
+$locator = $peerFactory->getLocator($connector, $dns);
 
-$local = new NetworkAddress(
-    Buffer::hex('01', 16),
-    '192.168.192.39',
-    32301
+$locator->queryDnsSeeds()->then(
+    function (\BitWasp\Bitcoin\Networking\P2P\PeerLocator $locator) use (&$loop) {
+        $locator->connectNextPeer()->then(
+            function (Peer $peer) use (&$loop) {
+                echo "connected to " . $peer->getRemoteAddr()->getIp() . "\n";
+                $loop->stop();
+            },
+            function ($error) {
+                throw $error;
+            }
+        );
+    }
 );
-
-$msgs = new MessageFactory(
-    Bitcoin::getDefaultNetwork(),
-    new BitWasp\Bitcoin\Crypto\Random\Random()
-);
-
-$peerFactory = new \BitWasp\Bitcoin\Networking\P2P\PeerFactory($local, $msgs, $loop);
-$locator = new PeerLocator(
-    $peerFactory,
-    $connector,
-    $dns
-);
-
-$locator->queryDnsSeeds()->then(function () use ($locator, &$loop) {
-    $locator->connectNextPeer()->then(function (Peer $peer) use (&$loop) {
-        echo "connected to " . $peer->getRemoteAddr()->getIp() . "\n";
-        $loop->stop();
-    }, function ($error) {
-        throw $error;
-    });
-});
 
 $loop->run();

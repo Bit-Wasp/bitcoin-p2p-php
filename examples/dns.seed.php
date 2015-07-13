1cf9ddf915a67199ba6f4c781f9a72d8c34bb99e
Thomas Kerin <afk11@users.noreply.github.com>
2015-07-14 00:08:17 +0100

    tweak dns seed example

--- examples/dns.seed.php
+++ examples/dns.seed.php
@@ -5,12 +5,13 @@ require_once "../vendor/autoload.php";
 $loop = React\EventLoop\Factory::create();
 
 $factory = new \BitWasp\Bitcoin\Networking\Factory($loop);
-$dns = $factory->getDns();
-
-$dns->resolve('dnsseed.bitcoin.dashjr.org')->then(
-    function ($ips) {
-        print_r($ips);
-    }
-);
+$factory
+    ->getDns()
+    ->resolve('dnsseed.bitcoin.dashjr.org')
+    ->then(
+        function ($ips) {
+            print_r($ips);
+        }
+    );
 
 $loop->run();

ad13bc433820064e82c1ed9a5bea6dd709ae22c9
Thomas Kerin <afk11@users.noreply.github.com>
2015-07-13 21:45:08 +0100

    Add some more methods to PeerFactory

--- src/P2P/PeerFactory.php
+++ src/P2P/PeerFactory.php
@@ -2,11 +2,14 @@
 
 namespace BitWasp\Bitcoin\Networking\P2P;
 
+use BitWasp\Bitcoin\Networking\Dns\Resolver;
 use BitWasp\Bitcoin\Networking\MessageFactory;
 use BitWasp\Bitcoin\Networking\Structure\NetworkAddress;
 use BitWasp\Bitcoin\Networking\Structure\NetworkAddressInterface;
 use BitWasp\Buffertools\Buffer;
 use React\EventLoop\LoopInterface;
+use React\Socket\Server;
+use React\SocketClient\Connector;
 
 class PeerFactory
 {
@@ -65,5 +68,38 @@ class PeerFactory
             $this->msgs,
             $this->loop
         );
+    }
+
+    /**
+     * @param Connector $connector
+     * @param Resolver $dns
+     * @return PeerLocator
+     */
+    public function getLocator(Connector $connector, Resolver $dns)
+    {
+        return new PeerLocator(
+            $this,
+            $connector,
+            $dns
+        );
+    }
+
+    /**
+     * @param Connector $connector
+     * @param Resolver $dns
+     * @return PeerManager
+     */
+    public function getManager(Connector $connector, Resolver $dns)
+    {
+        return new PeerManager($this->getLocator($connector, $dns));
+    }
+
+    /**
+     * @param Server $server
+     * @return Listener
+     */
+    public function getListener(Server $server)
+    {
+        return new Listener($this->local, $this->msgs, $server, $loop);
     }
 }

19b8ca6e02f53da2d16421368d019babda2405a1
Thomas Kerin <afk11@users.noreply.github.com>
2015-07-14 01:15:01 +0100

    Add quick test for Networking\Factory

---
+++ tests/FactoryTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BitWasp\Bitcoin\Tests\Networking;
+
+
+use BitWasp\Bitcoin\Networking\Factory;
+
+class FactoryTest extends AbstractTestCase
+{
+
+    public function testMethods()
+    {
+        $loop = new \React\EventLoop\StreamSelectLoop();
+        $factory = new Factory($loop);
+        $dns = $factory->getDns();
+        $this->assertInstanceOf('BitWasp\Bitcoin\Networking\Dns\Resolver', $dns);
+        $this->assertInstanceOf('BitWasp\Bitcoin\Networking\Messages\Factory', $factory->getMessages());
+        $this->assertInstanceOf('BitWasp\Bitcoin\Networking\Peer\Factory', $factory->getPeerFactory($dns));
+    }
+}

956036468c26f86437d5cae48c75e64942a46a95
Thomas Kerin <afk11@users.noreply.github.com>
2016-03-13 15:19:21 +0000

    Add Messages class, with msg name constants

---
+++ src/Messages.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BitWasp\Bitcoin\Networking;
+
+class Messages
+{
+    const ADDR = 'addr';
+    const ALERT = 'alert';
+    const BLOCK = 'block';
+    const FEEFILTER = 'feefilter';
+    const FILTERADD = 'filteradd';
+    const FILTERCLEAR = 'filterclear';
+    const FILTERLOAD = 'filterload';
+    const GETADDR = 'getaddr';
+    const GETBLOCKS = 'getblocks';
+    const GETDATA = 'getdata';
+    const GETHEADERS = 'getheaders';
+    const HEADERS = 'headers';
+    const INV = 'inv';
+    const MEMPOOL = 'mempool';
+    const MERKLEBLOCK = 'merkleblock';
+    const NOTFOUND = 'notfound';
+    const PING = 'ping';
+    const PONG = 'pong';
+    const REJECT = 'reject';
+    const SENDHEADERS = 'sendheaders';
+    const TX = 'tx';
+    const VERACK = 'verack';
+    const VERSION = 'version';
+}

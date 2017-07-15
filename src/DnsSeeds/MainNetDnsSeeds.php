3b533ee0d6946a02dcdc071f84c7a5e188bc259b
Thomas Kerin <thomas.kerin@bitmaintech.com>
2017-07-15 22:23:31 +0200

    deal with some random connection errors

--- src/DnsSeeds/MainNetDnsSeeds.php
+++ src/DnsSeeds/MainNetDnsSeeds.php
@@ -8,11 +8,11 @@ class MainNetDnsSeeds extends DnsSeedList
     public function __construct()
     {
         parent::__construct([
-            //'seed.bitcoin.sipa.be',
-            //'dnsseed.bluematt.me',
+            'seed.bitcoin.sipa.be',
+            'dnsseed.bluematt.me',
             'dnsseed.bitcoin.dashjr.org',
             'seed.bitcoinstats.com',
-            'bitseed.xf2.org',
+//            'bitseed.xf2.org',
             'seed.bitnodes.io',
             "seed.bitcoin.jonasschnelli.ch"
         ]);
